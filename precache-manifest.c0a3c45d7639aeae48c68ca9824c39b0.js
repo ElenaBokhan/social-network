0a9fad9c69d5b7c79999623ceb47bb82019cf7d2
Elena_Bokhan <clevergirl_777@mail.ru>
2020-08-08 22:53:32 +0400

    Updates

---
+++ precache-manifest.c0a3c45d7639aeae48c68ca9824c39b0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bc47357c40668c61c241c203f38d88",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "434d98b3e4b2d5c28d61",
+    "url": "/social-network/static/css/main.f98eb132.chunk.css"
+  },
+  {
+    "revision": "f97e91bd4951f6dcdd2a",
+    "url": "/social-network/static/js/2.8c59262a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/2.8c59262a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434d98b3e4b2d5c28d61",
+    "url": "/social-network/static/js/main.bd404572.chunk.js"
+  },
+  {
+    "revision": "9d5eaa9a4eccf0fde198",
+    "url": "/social-network/static/js/runtime-main.c37dcda3.js"
+  },
+  {
+    "revision": "18896efb417c895c49021f0decbd1ff0",
+    "url": "/social-network/static/media/1.18896efb.jpg"
+  },
+  {
+    "revision": "a39e2173afbf943d4ee2813d4e17042b",
+    "url": "/social-network/static/media/2.a39e2173.jpg"
+  },
+  {
+    "revision": "80e11b80dfa0891d6774009b71e8e077",
+    "url": "/social-network/static/media/3.80e11b80.jpg"
+  },
+  {
+    "revision": "f596c1fc7b48c0b15c87a7cc3163e087",
+    "url": "/social-network/static/media/4.f596c1fc.jpg"
+  },
+  {
+    "revision": "bcd0de1628c211715748ebc793c41138",
+    "url": "/social-network/static/media/Intro-Black.bcd0de16.otf"
+  },
+  {
+    "revision": "ff4d34f668bab5b2028b4c30c9c20851",
+    "url": "/social-network/static/media/Intro-Bold.ff4d34f6.otf"
+  },
+  {
+    "revision": "836f6592c133dc086333e2d03271d822",
+    "url": "/social-network/static/media/Intro-Light.836f6592.otf"
+  },
+  {
+    "revision": "2f11cb83f721271ebebd8f32af8d2068",
+    "url": "/social-network/static/media/Intro-Regular.2f11cb83.otf"
+  },
+  {
+    "revision": "81c06279ec284266c79ad4a2b33c01dc",
+    "url": "/social-network/static/media/avatar-auth.81c06279.jpg"
+  }
+]);
